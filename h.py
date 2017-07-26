d04fc9825b9593652af4f341950e8c3382ceb906
Dooha <kdha0924@gmail.com>
2017-07-26 23:47:58 +0900

    nothing changed

--- h.py
+++ h.py
@@ -4,13 +4,14 @@ import csv
 
 data = urlopen('http://doohaproject.tistory.com/attachment/cfile4.uf@259065335977804F2E3809.csv').read().decode()
 dataFile = StringIO(data)
-#csvReader = csv.reader(dataFile)
-csvDictReader = csv.DictReader(dataFile)
+dataFile2 = StringIO(data)
+csvReader = csv.reader(dataFile)
+csvDictReader = csv.DictReader(dataFile2)
 
-#for row in csvReader:
-#    print(row)
+for row in csvReader:
+    print(row)
 
+#csvDictReader 사용시 fieldname을 분리할 수 있음.
 print(csvDictReader.fieldnames)
-print('----------------')
 for row in csvDictReader:
     print(row)
